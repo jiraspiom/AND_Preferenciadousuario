ff1617c921de4b42ef64728a4c502bb1e8489c0e
gilmar <jiraspiom@yahoo.com.br>
2018-09-14 14:40:26 -0300

    Criado codigo que salva no arquivo de preferencia como privado, que so sera lido o xml pelo proprio aplicativo.

--- app/src/main/java/br/com/example/alalgi/preferenciadousuario/MainActivity.java
+++ app/src/main/java/br/com/example/alalgi/preferenciadousuario/MainActivity.java
@@ -1,13 +1,47 @@
 package br.com.example.alalgi.preferenciadousuario;
 
+import android.content.SharedPreferences;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSalvar;
+    private TextInputEditText editNome;
+    private TextView textResultado;
+    private final static String ARQUIVO_PREFRENCIA = "Arquivo preferencia";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSalvar = findViewById(R.id.buttonSalvar);
+        editNome = findViewById(R.id.textInputNome);
+        textResultado = findViewById(R.id.textInputNome);
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFRENCIA, 0);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                if(editNome.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"Preencha o campo nome", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    String nome = editNome.getText().toString();
+                    editor.commit();
+                }
+
+            }
+        });
+
     }
 }
